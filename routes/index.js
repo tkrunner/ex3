07083ed15728439bc01208342de1d350b99e28e9
saharat wongyai <tkrunner30102@gmail.com>
2019-07-13 16:07:57 +0700

    แสดง

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var connect = require('../config/database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'โคตรงง' });
+  var query = "SELECT * FROM positions";
+  connect.query(query, function(error, result){
+  res.json(result)
+});
 });
 
 router.get('/about', function(req, res, next) {
